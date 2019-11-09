fefc6c32f30c329a8d07b3b49b9ae191277e064a
asahyo <56744354+asahyo@users.noreply.github.com>
2019-11-08 15:03:50 -1000

    Add files via upload

--- hotelproject.js
+++ hotelproject.js
@@ -1,27 +1,32 @@
+//create global varieble for each room tha keeps track of how many people are in the room
+//add +1 to global var in the checkIn function
+//-1 from the global var in the checkOut funcion
 var i = 0;
 var x = document.getElementsByTagName('ol');
 var isVisible = true;
 
 var date = new Date();
+//sets each array number to the getMonth
 var m = ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
 var month = m[date.getMonth()];
-
-var n = d.getDate();
-document.getElementById("demo").innerHTML = n;
-
+var w = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var weekday = w[date.getDay()];
+var d = new Date();
+var date = d.getDate();
 
 function checkIn(){
   var newList = document.createElement('li');
   var guest = document.getElementById('checkin').value;
-  var x = document.createTextNode(guest+" "+month);
+  var x = document.createTextNode(guest+" - "+month+", "+weekday+" "+date);
   newList.appendChild(x);
   var position = document.getElementsByTagName('ol')[0];
   document.getElementById('checkin').value = " ";
-
+  //if less than 4 guests keep on append
     if(i < 4){
       i++;
       position.appendChild(newList);
     }
+    //if greater than 4 guests, room is full
     else{
       document.getElementById('fullroom').innerHTML = "room is full!!";
     }
@@ -29,12 +34,15 @@ function checkIn(){
 }
 
 function checkOut(){
+  i--;
   var userNum = document.getElementById('checkout').value;
   var y = parseInt(userNum);
   var child = document.getElementsByTagName('li')[y-1];
   var parent = child.parentNode;
   parent.removeChild(child);
+  //when check out, make sure "room is full" is erased
   document.getElementById('checkout').value = " ";
+  //when clear room, make sure "room is full" is erased
   document.getElementById('fullroom').innerHTML = " ";
   
 }
@@ -47,6 +55,7 @@ function clearAll(){
   document.getElementById('fullroom').innerHTML = " ";
 }
 
+//lock room button makes the checkin button hidden and visible
 function lockRoom(){
   if (isVisible == true){
     document.getElementById('lock').style.visibility = "hidden";
